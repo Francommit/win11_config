1d8109fc378d2b27d0cf22a98f613bf3e000702d
Chris Franco <11533365+Francommit@users.noreply.github.com>
2022-02-23 13:40:06 +1100

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,4 +1,5 @@
 # Shared tools
+winget install git.git --accept-source-agreements
 winget install 7zip.7zip --accept-source-agreements
 winget install Lexikos.AutoHotkey --accept-source-agreements
 winget install voidtools.Everything --accept-source-agreements
@@ -6,6 +7,7 @@ winget install Microsoft.VisualStudioCode --accept-source-agreements
 winget install Microsoft.WindowsTerminal --accept-source-agreements
 winget install SublimeHQ.SublimeText.4 --accept-source-agreements
 winget install WinSCP.WinSCP --accept-source-agreements
+winget install Google.Chrome --accept-source-agreements
 
 # Personal tools
 winget install GitHub.GitHubDesktop --accept-source-agreements

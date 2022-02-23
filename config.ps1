53c0a9887ba3b19066f0ed53cd64b2ee31d08897
Chris Franco <11533365+Francommit@users.noreply.github.com>
2022-02-23 13:35:16 +1100

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,1 +1,13 @@
-Write-Host "Hello world"
+# Shared tools
+winget install 7zip.7zip --accept-source-agreements
+winget install Lexikos.AutoHotkey --accept-source-agreements
+winget install voidtools.Everything --accept-source-agreements
+winget install Microsoft.VisualStudioCode --accept-source-agreements
+winget install Microsoft.WindowsTerminal --accept-source-agreements
+winget install SublimeHQ.SublimeText.4 --accept-source-agreements
+
+# Personal tools
+winget install GitHub.GitHubDesktop --accept-source-agreements
+
+# Games
+winget install Valve.Steam --accept-source-agreements
